c97466e39e407aa59b44886aeb2095cea4d4aabb
Benglennon123 <glennonben@gmail.com>
2023-11-20 15:09:38 +0000

    switching to dash

--- demo.py
+++ demo.py
@@ -1,87 +1,155 @@
+import os
+import time
+from textwrap import dedent
+
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output, State
+from PIL import Image
 import openai
-import streamlit as st
-
-# Set up Session State
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
-if "primer" not in st.session_state:
-    st.session_state["primer"] = "You are a friendly and helpful assistant."
-if "context_length" not in st.session_state:
-    st.session_state["context_length"] = 10
 
 
-def main():
-    # Initialization your state messages
+def Header(name, app):
+    title = html.H1(name, style={"margin-top": 5})
+    logo = html.Img(
+        src=app.get_asset_url("dash-logo.png"), style={"float": "right", "height": 60}
+    )
+    return dbc.Row([dbc.Col(title, md=8), dbc.Col(logo, md=4)])
 
-    st.sidebar.header("Settings")
 
-    with st.sidebar:
-        # Allow the user to set their prompt
-        st.session_state.primer = st.text_area(
-            "Primer Message",
-            "You are a friendly and helpful assistant.",
+def textbox(text, box="AI", name="Philippe"):
+    text = text.replace(f"{name}:", "").replace("You:", "")
+    style = {
+        "max-width": "60%",
+        "width": "max-content",
+        "padding": "5px 10px",
+        "border-radius": 25,
+        "margin-bottom": 20,
+    }
+
+    if box == "user":
+        style["margin-left"] = "auto"
+        style["margin-right"] = 0
+
+        return dbc.Card(text, style=style, body=True, color="primary", inverse=True)
+
+    elif box == "AI":
+        style["margin-left"] = 0
+        style["margin-right"] = "auto"
+
+        thumbnail = html.Img(
+            src=app.get_asset_url("Philippe.jpg"),
+            style={
+                "border-radius": 50,
+                "height": 36,
+                "margin-right": 5,
+                "float": "left",
+            },
         )
-        st.session_state.context_length = st.slider(
-            "Context Message Length", min_value=1, max_value=50, value=10, step=1
-        )
+        textbox = dbc.Card(text, style=style, body=True, color="light", inverse=False)
 
-        # Allow Users to reset the memory
-        if st.button("Clear Chat"):
-            st.session_state.messages = []
-            st.info("Chat Memory Cleared")
+        return html.Div([thumbnail, textbox])
 
-    # A place to draw the chat history
-    history = st.container()
-
-    with st.form("Chat"):
-        input = st.text_input("You:", "")
-        if st.form_submit_button():
-            st.session_state.messages.append({"role": "user", "content": input})
-
-            # Create an on the fly message stack
-            messages = [{"role": "system", "content": st.session_state.primer}]
-            messages.extend(
-                st.session_state.messages[-st.session_state.context_length :]
-            )
-
-            # Call the OpenAI API
-            r = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
-            tokens = r["usage"]["total_tokens"]
-            cost = round((tokens / 1000) * 0.02, 3)
-            st.info(f"Message uses {tokens} tokens for a total cost of {cost} cents")
-
-            with st.expander("Result"):
-                st.info("Your Output Response")
-                st.write(r)
-
-            st.session_state.messages.append(
-                {"role": "assistant", "content": r["choices"][0]["message"]["content"]}
-            )
-
-    with history:
-        for i, message in enumerate(st.session_state.messages):
-            c1, c2 = st.columns([2, 10])
-            with c1:
-                st.write(message["role"])
-            with c2:
-                # Lets italisize the messages that are sent in the state
-                if (
-                    len(st.session_state.messages) - i
-                    < st.session_state.context_length + 1
-                ):
-                    st.markdown(f'_{message["content"]}_')
-                else:
-                    st.markdown(f'{message["content"]}')
+    else:
+        raise ValueError("Incorrect option for `box`.")
 
 
-st.title("Open AI Chat GPT Demo")
+# Authentication
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-key = st.text_input("Your Open API Key", "sk...")
-if key == "sk...":
-    st.error("Please add a valid Open API Key")
-
-else:
-    openai.api_key = key
-    main()
+# Define app
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
 
 
+# Load images
+IMAGES = {"Philippe": app.get_asset_url("Philippe.jpg")}
+
+
+# Define Layout
+conversation = html.Div(
+    html.Div(id="display-conversation"),
+    style={
+        "overflow-y": "auto",
+        "display": "flex",
+        "height": "calc(90vh - 132px)",
+        "flex-direction": "column-reverse",
+    },
+)
+
+controls = dbc.InputGroup(
+    children=[
+        dbc.Input(id="user-input", placeholder="Write to the chatbot...", type="text"),
+        dbc.InputGroupAddon(dbc.Button("Submit", id="submit"), addon_type="append"),
+    ]
+)
+
+app.layout = dbc.Container(
+    fluid=False,
+    children=[
+        Header("Dash GPT-3 Chatbot", app),
+        html.Hr(),
+        dcc.Store(id="store-conversation", data=""),
+        conversation,
+        controls,
+        dbc.Spinner(html.Div(id="loading-component")),
+    ],
+)
+
+
+@app.callback(
+    Output("display-conversation", "children"), [Input("store-conversation", "data")]
+)
+def update_display(chat_history):
+    return [
+        textbox(x, box="user") if i % 2 == 0 else textbox(x, box="AI")
+        for i, x in enumerate(chat_history.split("<split>")[:-1])
+    ]
+
+
+@app.callback(
+    Output("user-input", "value"),
+    [Input("submit", "n_clicks"), Input("user-input", "n_submit")],
+)
+def clear_input(n_clicks, n_submit):
+    return ""
+
+
+@app.callback(
+    [Output("store-conversation", "data"), Output("loading-component", "children")],
+    [Input("submit", "n_clicks"), Input("user-input", "n_submit")],
+    [State("user-input", "value"), State("store-conversation", "data")],
+)
+def run_chatbot(n_clicks, n_submit, user_input, chat_history):
+    if n_clicks == 0 and n_submit is None:
+        return "", None
+
+    if user_input is None or user_input == "":
+        return chat_history, None
+
+    # First add the user input to the chat history
+    chat_history += f"You: {user_input}<split>:"
+
+    model_input = chat_history.replace("<split>", "\n")
+
+    response = openai.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair.",
+            },
+            {"role": "user", "content": model_input},
+        ],
+    )
+    model_output = response.choices[0].message.content.strip()
+
+    chat_history += f"{model_output}<split>"
+
+    return chat_history, None
+
+
+if __name__ == "__main__":
+    app.run_server(debug=False)
